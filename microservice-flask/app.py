ddfb722196c4001d474e2091346cd112b7bccbf5
Leonardo Rojas <119978221+leorjs@users.noreply.github.com>
2025-02-21 19:10:04 -0300

    Update app.py

--- microservice-flask/app.py
+++ microservice-flask/app.py
@@ -1,1 +1,10 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Microservicio en Flask funcionando correctamente"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
